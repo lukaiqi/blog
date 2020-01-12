d51bd1ec83a62542dfb742c0cf9f89f4f2bf12be
lukaiqi <1263041461@qq.com>
2020-01-12 15:33:37 +0800

    页面

--- ssz/views.py
+++ ssz/views.py
@@ -12,7 +12,7 @@ class StandardResultsSetPagination(PageNumberPagination):
     """
     自定义分页属性
     """
-    page_size = 10
+    page_size = 15
     # page_query_param = 'p' #页码名称
     page_size_query_param = 'page_size'
     max_page_size = 100
@@ -35,6 +35,6 @@ class QmViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.Generic
     """
     authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
-    serializer_class = BiliBiliSerializer
-    queryset = Bilibili.objects.all()
+    serializer_class = QQMusicSerializer
+    queryset = QQMusic.objects.all()
     pagination_class = StandardResultsSetPagination
