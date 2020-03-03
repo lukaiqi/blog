0e3c1516f5d3ffc6254ea008f0b54dde472a6549
lukaiqi <1263041461@qq.com>
2020-03-03 14:14:58 +0800

    新增访客IP定位

--- client/models.py
+++ client/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Client(models.Model):
     ip = models.CharField(max_length=20, default='', verbose_name='IP')
+    addr = models.CharField(max_length=20, default='', verbose_name='定位')
     path = models.CharField(max_length=50, default='', verbose_name='访问路径')
     add_time = models.DateTimeField(auto_now_add=True, verbose_name='访问时间')
 
--- utils/clientip.py
+++ utils/clientip.py
@@ -1,7 +1,7 @@
 from django.utils.deprecation import MiddlewareMixin
 from client.models import Client
-
-visit_ip = ''
+import requests
+from bs4 import BeautifulSoup
 
 
 class ClientIpMiddleware(MiddlewareMixin):
@@ -12,10 +12,25 @@ class ClientIpMiddleware(MiddlewareMixin):
             ip = request.META['HTTP_X_FORWARDED_FOR']
         else:
             ip = request.META['REMOTE_ADDR']
+        # 通过ip定位
+        try:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+            }
+            res = requests.get('http://ip138.com/iplookup.asp?ip={}&action=2'.format(ip), headers=headers)
+            # 解析返回的网页
+            html = BeautifulSoup(res.content.decode('gb2312'), 'lxml')
+            # 提取数据
+            li = html.select('.ul1 li')[0].text
+            addr = li.split('：')[1]
+        except Exception as e:
+            addr = '获取失败'
         # 获取访问路径
         path = request.path_info
-        client = Client()
-        client.ip = ip
-        client.path = path
-        client.save()
+        if 'xadmin' not in path:
+            client = Client()
+            client.ip = ip
+            client.path = path
+            client.addr = addr
+            client.save()
         return None
