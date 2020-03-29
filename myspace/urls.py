0acb70d0fd595ecca4170556c0741da88c8966f7
lukaiqi <1263041461@qq.com>
2020-03-29 09:06:02 +0800

    删除sensor和bilibi的url

--- myspace/urls.py
+++ myspace/urls.py
@@ -22,8 +22,6 @@ from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
 from article.views import ArticleListViewSet, CommentViewSet
 from users.views import UserViewSet, SMSCodeViewSet, QqLogin, OauthBindViewSet, MpLogin, WbLogin
-from sensor.views import SensorViewSet
-from bilibili.views import VideoListViewSet, UserInfoViewSet
 from client.views import ClientViewSet
 
 router = SimpleRouter()
@@ -32,9 +30,6 @@ router.register('send_code', SMSCodeViewSet, base_name='send_code'),
 router.register('user', UserViewSet, base_name='user'),
 router.register('comment', CommentViewSet, base_name='comment'),
 router.register('oauthbind', OauthBindViewSet, base_name='oauthbind'),
-router.register('sensor', SensorViewSet, base_name='sensor'),
-router.register('videolist', VideoListViewSet, base_name='videolist'),
-router.register('bili_info', UserInfoViewSet, base_name='bili_info'),
 router.register('clientip', ClientViewSet, base_name='clientip')
 
 urlpatterns = [
